a706e9e1a66d5845ef413c6b2b9e031680e3ac09
Muh-Hasan <muh.hasan.git@gmail.com>
2020-08-25 08:30:08 -0700

    few changes

--- src/components/Launch/launch.tsx
+++ src/components/Launch/launch.tsx
@@ -18,27 +18,29 @@ const Launch: React.FC<Props> = ({ data }) => {
     const displayData = data.launches?.map((launch, i) => {
         return (
             <div key={launch?.flight_number} className='launch-box'>
-                <div className='launch-mission'>
-                    <span className='sp-one'>{`${launch?.flight_number}. `}</span>
-                    <Link to={`launch/${launch?.flight_number}`}>
-                    <span className='sp-two'>Mission: {launch?.mission_name}</span>
-                    </Link>
-                </div>
-                <div className='launch-site'>
-                    <span className='sp-one'>LaunchSite: </span><span>{launch?.launch_site?.site_name}</span>
-                </div>
-                <div className='launch-site'>
-                    <span className='sp-one'>Launch year: </span><span>{launch?.launch_year}</span>
-                </div>
-                <div className='launch-status'>
-                    <span className='sp-one'>status: <span className={launch?.launch_success ? 'true' : 'false'}>{launch?.launch_success ? 'sucessful' : 'failed'}</span></span>
-                </div>
-                <div className='launch-btu'>
-                    <Link to={`launch/${launch?.flight_number}`}>
-                        <button className='hvr-bounce-to-bottom launch-btn'>
-                            <span>More</span>
-                        </button>
-                    </Link>
+                <div className='launch-margin'>
+                    <div className='launch-mission'>
+                        <span className='sp-one'>{`${launch?.flight_number}. `}</span>
+                        <Link to={`launch/${launch?.flight_number}`}>
+                            <span className='sp-two'>Mission: {launch?.mission_name}</span>
+                        </Link>
+                    </div>
+                    <div className='launch-site'>
+                        <span className='sp-one'>LaunchSite: </span><span>{launch?.launch_site?.site_name}</span>
+                    </div>
+                    <div className='launch-site'>
+                        <span className='sp-one'>Launch year: </span><span>{launch?.launch_year}</span>
+                    </div>
+                    <div className='launch-status'>
+                        <span className='sp-one'>status: <span className={launch?.launch_success ? 'true' : 'false'}>{launch?.launch_success ? 'sucessful' : 'failed'}</span></span>
+                    </div>
+                    <div className='launch-btu'>
+                        <Link to={`launch/${launch?.flight_number}`}>
+                            <button className='hvr-bounce-to-bottom launch-btn'>
+                                <span>details</span>
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         )
